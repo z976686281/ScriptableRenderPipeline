8ba6c56a7553e77d250fe59476b09132d1791ec5
Tristan Genevet <tristan@unity3d.com>
2018-11-19 16:34:42 +0100

    fix for creating and pasting operators and blocks with custom attributes.

--- com.unity.visualeffectgraph/Editor/Core/VFXLibrary.cs
+++ com.unity.visualeffectgraph/Editor/Core/VFXLibrary.cs
@@ -73,7 +73,7 @@ namespace UnityEditor.VFX
             return parent.AcceptChild(m_Template, index);
         }
 
-        protected void ApplyVariant(VFXModel model)
+        public void ApplyVariant(VFXModel model)
         {
             foreach (var variant in m_Variants)
             {
